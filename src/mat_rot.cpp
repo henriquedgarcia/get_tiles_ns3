5a77a5ba614b7697cab3a9a3afcf724288be0917
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2026-02-04 14:55:27 -0300

    Update mat_rot.cpp

--- src/mat_rot.cpp
+++ src/mat_rot.cpp
@@ -1,5 +1,6 @@
 #include "mat_rot.h"
 #include "types.h"
+#include <ranges>
 
 namespace MatRot
 {
@@ -23,12 +24,12 @@ namespace MatRot
         :return: A 3x3 matrix of rotation
         */
 
-        float cos_yaw = cos(yaw_pitch_roll.yaw);
-        float cos_pitch = cos(yaw_pitch_roll.pitch);
-        float cos_roll = cos(yaw_pitch_roll.roll);
-        float sin_yaw = sin(yaw_pitch_roll.yaw);
-        float sin_pitch = sin(yaw_pitch_roll.pitch);
-        float sin_roll = sin(yaw_pitch_roll.roll);
+        double cos_yaw = cos(yaw_pitch_roll.yaw);
+        double cos_pitch = cos(yaw_pitch_roll.pitch);
+        double cos_roll = cos(yaw_pitch_roll.roll);
+        double sin_yaw = sin(yaw_pitch_roll.yaw);
+        double sin_pitch = sin(yaw_pitch_roll.pitch);
+        double sin_roll = sin(yaw_pitch_roll.roll);
 
         Mat3 mat_x{{{1, 0, 0},
                     {0, cos_pitch, -sin_pitch},
@@ -42,39 +43,6 @@ namespace MatRot
 
         Mat3 mat_zy = mult2(mat_z, mat_y);
         return mult2(mat_zy, mat_x);
-    }
-
-    inline Mat3 mult2(const Mat3 &A, const Mat3 &B)
-    {
-        /* Multiply two 3x3 matrices A and B */
-        Mat3 C{};
-        for (int i = 0; i < 3; ++i)
-        {
-            for (int j = 0; j < 3; ++j)
-            {
-                C[i][j] = A[i][0] * B[0][j] + A[i][1] * B[1][j] + A[i][2] * B[2][j];
-            }
-        }
-        return C;
-    }
-
-    inline Point3D apply_matrix(const Mat3 &mat, const Point3D &p)
-    {
-        /* Apply matrix M to point p */
-        float x = mat[0][0] * p.x + mat[0][1] * p.y + mat[0][2] * p.z;
-        float y = mat[1][0] * p.x + mat[1][1] * p.y + mat[1][2] * p.z;
-        float z = mat[2][0] * p.x + mat[2][1] * p.y + mat[2][2] * p.z;
-        return Point3D(x, y, z);
-    }
-
-    inline Frustrum rotate_frustrum(Frustrum frustrum, const Mat3 &mat)
-    {
-        /* Rotate the normals of the frustrum f using matrix M */
-        frustrum.normals[0] = apply_matrix(mat, frustrum.normals[0]);
-        frustrum.normals[1] = apply_matrix(mat, frustrum.normals[1]);
-        frustrum.normals[2] = apply_matrix(mat, frustrum.normals[2]);
-        frustrum.normals[3] = apply_matrix(mat, frustrum.normals[3]);
-        return frustrum;
     }
 
 } // namespace MatRot
