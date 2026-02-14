6e0d0555a7ef07c793a510317fb4096380823578
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2026-02-13 23:46:01 -0300

    Update utils.h

--- include/utils.h
+++ include/utils.h
@@ -1,28 +1,19 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-#include <numbers> // pi
-#include <vector>
-#include <numeric> // std::iota
 #include <cmath>
+#include <numbers> // pi
+#include <numeric> // std::iota
+#include <vector>
 
 #include "types.h" // Point3D
 
-inline double deg2rad(double deg) {
-    return deg * std::numbers::pi / 180.0;
-}
+inline double deg2rad(double deg) { return deg * std::numbers::pi / 180.0; }
 
-inline double rad2deg(double rad) {
-    return rad * 180.0 / std::numbers::pi;
-}
+inline double rad2deg(double rad) { return rad * 180.0 / std::numbers::pi; }
 
-std::vector<Point3D> predict_future(const std::vector<Point3D> &data, int n_future = 30);
-std::pair<double,double> linear_regression(const std::vector<double>& y);
-
-
-
-
-
-
+std::vector<Point3D> predict_future(const std::vector<Point3D>& data,
+                                    int n_future = 30);
+std::pair<double, double> linear_regression(const std::vector<double>& y);
 
 #endif // UTILS_H
