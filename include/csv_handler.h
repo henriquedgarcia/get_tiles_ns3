2726d14eb95017843860481abf2675d1c172f33b
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2026-03-02 16:16:23 -0300

    Update csv_handler.h

--- include/csv_handler.h
+++ include/csv_handler.h
@@ -2,24 +2,24 @@
 #define CSV_HANDLER_H
 
 #include "types.h"
-#include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
 
 class CsvHandler {
-  private:
-    const std::string filename;
-    const std::vector<std::string> columnTypes;
-    int write_header(std::ofstream& output_file_stream,
-                     const std::vector<std::string>& headers = {});
-    void write_csv_line(std::ofstream& out, const std::vector<Field>& values);
-
   public:
     CsvHandler(const std::string& filename,
                const std::vector<std::string>& columnTypes);
     int write(const std::vector<Row>& rows,
               const std::vector<std::string>& headers = {});
     std::vector<Row> read(bool skipHeader = true);
+
+    private:
+    const std::string filename;
+    const std::vector<std::string> columnTypes;
+    int write_header(std::ofstream& output_file_stream,
+                     const std::vector<std::string>& headers = {});
+    void write_csv_line(std::ofstream& out, const std::vector<Field>& values);
 };
 
 #endif // CSV_HANDLER_H
