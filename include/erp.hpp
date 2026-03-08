8df19a5fa3f792b608f098d29fc30ff695324127
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2026-03-08 15:01:46 -0300

    Update erp.hpp

--- include/erp.hpp
+++ include/erp.hpp
@@ -8,9 +8,12 @@ class ERP : public Projection
 public:
     using Projection::Projection;
     Point3D mn2xyz(const ImagePoint &mn) const override;
+    // ImagePoint xyz2mn(const Point3D &xyz) const override;
+
     UvPoint mn2uv(const ImagePoint &mn) const;
     AePoint uv2ae(const UvPoint &uv) const;
     Point3D ae2xyz(const AePoint &ae) const;
+    // todo: add uv2mn, ae2uv, xyz2ae
 };
 
 #endif // ERP_H
