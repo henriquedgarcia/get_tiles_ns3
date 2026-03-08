4592b3a55584f196955aaa8515bcf1e54bb32497
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2026-03-08 15:01:59 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,13 @@
 #include "utils.hpp"
 #include <iostream>
 
+#include <filesystem>
+#include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+
+using namespace cv;
 using namespace std;
 
 // Configurações globais para os testes
@@ -23,12 +30,13 @@ class Test {
     void test_SeenTiles();
     void test_predictor();
     void test_CsvHandler();
+    void test_OpenCV();
 };
 
 int main() {
     Test test;
-    test.test_Projection();
-    test.test_SeenTiles();
+    test.test_OpenCV();
+    // test.test_SeenTiles();
 
     return 0;
 }
@@ -72,18 +80,35 @@ void Test::test_SeenTiles() {
 }
 
 void Test::test_predictor() {
-    //     std::vector<Point3D> data(30);
-    //     // preencher data com suas 30 amostras... carregar um arquivo,
-    //     pode ser um csv.
-    //      for (int i = 0; i < 30; ++i) {
-    //         data[i] = Point3D(i, i*2, i*3); // exemplo de dados
-    //     }
+    std::vector<Point3D> data(30);
+    // preencher data com suas 30 amostras... carregar um arquivo, pode ser um
+    // csv.
+    for (int i = 0; i < 30; ++i) {
+        data[i] = Point3D(i, i * 2, i * 3); // exemplo de dados
+    }
 
-    //     auto future = predict_future(data, 10); // prever os próximos 10
-    //     pontos
+    auto future = predict_future(data, 10); // prever os próximos 10 pontos
 
-    //     for (const auto& p : future) {
-    //         std::cout << p.x << ", " << p.y << ", " << p.z << "\n";
-    //     }
+    for (const auto& p : future) {
+        std::cout << p.x << ", " << p.y << ", " << p.z << "\n";
+    }
 }
+
 void Test::test_CsvHandler() {}
+
+void Test::test_OpenCV() {
+    std::string image_path = samples::findFile("starry_night.jpg");
+    Mat img = imread(image_path, IMREAD_COLOR);
+
+    if (img.empty()) {
+        std::cout << "Could not read the image: " << image_path << std::endl;
+        return;
+    }
+
+    // imshow("Display window", img);
+    // int k = waitKey(0); // Wait for a keystroke in the window
+
+    if (k == 's') {
+        imwrite("starry_night.png", img);
+    }
+}
