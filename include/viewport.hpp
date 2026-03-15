ad0ae47271bfa3d1fcc2659cb566cac629730482
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2026-03-15 18:58:49 -0300

    Update viewport.hpp

--- include/viewport.hpp
+++ include/viewport.hpp
@@ -2,35 +2,40 @@
 #define VIEWPORT_H
 
 #include <cmath>
-#include <numbers> // pi
-#include <numeric> // std::iota
+#include <numbers>  // pi
+#include <numeric>  // std::iota
+#include <opencv2/opencv.hpp>
 #include <vector>
 
-#include "types.hpp" // Point3D
-#include "projection.hpp" // Projection
+#include "projection.hpp"  // Projection
+#include "types.hpp"       // Point3D
 
 class Imagem;
 
-class GnomonicProjection : public Projection {
-  public:
-    Point3D mn2xyz(const ImagePoint &mn) const override;
-    // ImagePoint xyz2mn(const Point3D &xyz) const override;
-};
+// class GnomonicProjection : public Projection {
+//  public:
+//   Point3D mn2xyz(const ImagePoint& mn) const override;
+//   // ImagePoint xyz2mn(const Point3D &xyz) const override;
+// };
 
 class Viewport {
-  public:
-    Viewport(const Resolution& resolution, const Tiling& tiling);
-    // std::vector<Tile> tile_list;
-    // Resolution resolution;
-    // Resolution tile_resolution;
-    // Tiling tiling;
-    // int n_tiles;
+ private:
+  cv::Mat xyz_grid_default;
+  cv::Mat xyz_grid_rotated;
+  ViewportCoord yaw_pitch_roll;
 
-    // extract_viewport(self, proj_frame, yaw_pitch_roll=None) -> np.ndarray:
-    Imagem extract_viewport();
-    std::vector<ImagePoint> get_tile_borders(const Tile& tile);
+  void fill_xyz_grid_default();
 
-    virtual Point3D mn2xyz(const ImagePoint& mn) const = 0;
+ public:
+  const Resolution resolution;
+  const Fov fov;
+  const Projection* projection;
+
+  Viewport(const Resolution& resolution,
+           const Fov& fov,
+           const Projection* projection);
+
+  cv::Mat extract_viewport(cv::Mat proj_frame, ViewportCoord yaw_pitch_roll);
 };
 
-#endif // VIEWPORT_H
+#endif  // VIEWPORT_H
