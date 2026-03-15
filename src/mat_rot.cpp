2a518219875f467ff9ee77b144a1cb170662512d
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2026-03-15 18:58:54 -0300

    Update mat_rot.cpp

--- src/mat_rot.cpp
+++ src/mat_rot.cpp
@@ -1,48 +1,42 @@
 #include "mat_rot.hpp"
-#include "types.hpp"
+
+#include <opencv2/core.hpp>
 #include <ranges>
 
-namespace MatRot
-{
+#include "types.hpp"
 
-    Mat3 get_matrix(ViewportCoord yaw_pitch_roll)
-    {
-        /*
-        Create rotation matrix using Tait–Bryan angles in Z-Y-X order.
-        See Wikipedia. Use:
-            X axis point to right
-            Y axis point to down
-            Z axis point to front
+namespace MatRot {
 
-        Examples
-        --------
-        >> x, y, z = point
-        >> mat = rot_matrix([yaw, pitch, roll])
-        >> mat @ (x, y, z)
+cv::Mat get_matrix(ViewportCoord yaw_pitch_roll) {
+  /*
+  Create rotation matrix using Tait–Bryan angles in Z-Y-X order.
+  See Wikipedia. Use:
+      X axis point to right
+      Y axis point to down
+      Z axis point to front
 
-        :param yaw_pitch_roll: the rotation (yaw, pitch, roll) in rad.
-        :return: A 3x3 matrix of rotation
-        */
+  Examples
+  --------
+  >> x, y, z = point
+  >> mat = rot_matrix([yaw, pitch, roll])
+  >> mat @ (x, y, z)
 
-        double cos_yaw = cos(yaw_pitch_roll.yaw);
-        double cos_pitch = cos(yaw_pitch_roll.pitch);
-        double cos_roll = cos(yaw_pitch_roll.roll);
-        double sin_yaw = sin(yaw_pitch_roll.yaw);
-        double sin_pitch = sin(yaw_pitch_roll.pitch);
-        double sin_roll = sin(yaw_pitch_roll.roll);
+  :param yaw_pitch_roll: the rotation (yaw, pitch, roll) in rad.
+  :return: A 3x3 matrix of rotation
+  */
 
-        Mat3 mat_x{{{1, 0, 0},
-                    {0, cos_pitch, -sin_pitch},
-                    {0, sin_pitch, cos_pitch}}};
-        Mat3 mat_y{{{cos_yaw, 0, sin_yaw},
-                    {0, 1, 0},
-                    {-sin_yaw, 0, cos_yaw}}};
-        Mat3 mat_z{{{cos_roll, -sin_roll, 0},
-                    {sin_roll, cos_roll, 0},
-                    {0, 0, 1}}};
+  double cos_yaw = cos(yaw_pitch_roll.yaw);
+  double cos_pitch = cos(yaw_pitch_roll.pitch);
+  double cos_roll = cos(yaw_pitch_roll.roll);
+  double sin_yaw = sin(yaw_pitch_roll.yaw);
+  double sin_pitch = sin(yaw_pitch_roll.pitch);
+  double sin_roll = sin(yaw_pitch_roll.roll);
 
-        Mat3 mat_zy = mult2(mat_z, mat_y);
-        return mult2(mat_zy, mat_x);
-    }
+  cv::Mat mat_x = (cv::Mat_<double>(3, 3) << 1, 0, 0, 0, cos_pitch, -sin_pitch, 0, sin_pitch, cos_pitch);
+  cv::Mat mat_y = (cv::Mat_<double>(3, 3) << cos_yaw, 0, sin_yaw, 0, 1, 0, -sin_yaw, 0, cos_yaw);
+  cv::Mat mat_z = (cv::Mat_<double>(3, 3) << cos_roll, -sin_roll, 0, sin_roll, cos_roll, 0, 0, 0, 1);
 
-} // namespace MatRot
+  return mat_z * mat_y * mat_x;
+}
+
+}  // namespace MatRot
