5706ff1dfdc5a0b06c93c3dcb8c37b9ad9c305c3
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2026-03-15 18:58:35 -0300

    Update mat_rot.hpp

--- include/mat_rot.hpp
+++ include/mat_rot.hpp
@@ -1,47 +1,50 @@
 #ifndef MAT_ROT_H
 #define MAT_ROT_H
 
+#include <opencv2/core.hpp>
+
 #include "types.hpp"
 
-namespace MatRot
-{
-    Mat3 get_matrix(ViewportCoord yaw_pitch_roll);
+namespace MatRot {
+cv::Mat get_matrix(ViewportCoord yaw_pitch_roll);
 
-    inline Mat3 mult2(const Mat3 &A, const Mat3 &B)
-    {
-        /* Multiply two 3x3 matrices A and B */
-        Mat3 C{};
-        for (int i = 0; i < 3; ++i)
-        {
-            for (int j = 0; j < 3; ++j)
-            {
-                C[i][j] = A[i][0] * B[0][j] + A[i][1] * B[1][j] + A[i][2] * B[2][j];
-            }
-        }
-        return C;
-    }
+inline Point3D apply_matrix(const cv::Mat& mat, const Point3D& p) {
+  /* Apply matrix M to point p */
+  Point3D xyz;
+  xyz.x = mat.at<double>(0, 0) * p.x + mat.at<double>(0, 1) * p.y + mat.at<double>(0, 2) * p.z;
+  xyz.y = mat.at<double>(1, 0) * p.x + mat.at<double>(1, 1) * p.y + mat.at<double>(1, 2) * p.z;
+  xyz.z = mat.at<double>(2, 0) * p.x + mat.at<double>(2, 1) * p.y + mat.at<double>(2, 2) * p.z;
+  return xyz;
+}
 
-    inline Point3D apply_matrix(const Mat3 &mat, const Point3D &p)
-    {
-        /* Apply matrix M to point p */
-        Point3D xyz;
-        xyz.x = mat[0][0] * p.x + mat[0][1] * p.y + mat[0][2] * p.z;
-        xyz.y = mat[1][0] * p.x + mat[1][1] * p.y + mat[1][2] * p.z;
-        xyz.z = mat[2][0] * p.x + mat[2][1] * p.y + mat[2][2] * p.z;
-        return xyz;
-    }
+inline Frustrum rotate_frustrum(const Frustrum& frustrum, const cv::Mat& mat) {
+  Frustrum rotated = frustrum;
+  for (auto& normal : rotated.normals) {
+    normal = apply_matrix(mat, normal);
+  }
 
-    inline Frustrum rotate_frustrum(const Frustrum &frustrum, const Mat3 &mat)
-    {
+  return rotated;
+}
+
+class MatRot {
+public:
+    static cv::Mat get_matrix(const ViewportCoord& yaw_pitch_roll);
+
+    static Frustrum rotate_frustrum(const Frustrum& frustrum, const cv::Mat& mat) {
         Frustrum rotated = frustrum;
-        for (auto &normal : rotated.normals)
-        {
-            normal = apply_matrix(mat, normal);
+        for (auto& normal : rotated.normals) {
+            cv::Mat p = (cv::Mat_<double>(3,1) << normal.x, normal.y, normal.z);
+            cv::Mat r = mat * p;
+            normal.x = r.at<double>(0,0);
+            normal.y = r.at<double>(1,0);
+            normal.z = r.at<double>(2,0);
         }
-
         return rotated;
     }
+};
 
-} // namespace MatRot
 
-#endif // MAT_ROT_H
+
+}  // namespace MatRot
+
+#endif  // MAT_ROT_H
