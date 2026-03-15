d26b7e8324f84d74e7c5b8858d45ee94fa1f0315
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2026-03-15 18:58:33 -0300

    Update erp.hpp

--- include/erp.hpp
+++ include/erp.hpp
@@ -3,17 +3,19 @@
 
 #include "projection.hpp"
 
-class ERP : public Projection
-{
-public:
-    using Projection::Projection;
-    Point3D mn2xyz(const ImagePoint &mn) const override;
-    // ImagePoint xyz2mn(const Point3D &xyz) const override;
+class ERP : public Projection {
+ public:
+  using Projection::Projection;
+  Point3D mn2xyz(const ImagePoint& mn) const override;
+  ImagePoint xyz2mn(const Point3D& xyz) const override;
 
-    UvPoint mn2uv(const ImagePoint &mn) const;
-    AePoint uv2ae(const UvPoint &uv) const;
-    Point3D ae2xyz(const AePoint &ae) const;
-    // todo: add uv2mn, ae2uv, xyz2ae
+  UvPoint mn2uv(const ImagePoint& mn) const;
+  AePoint uv2ae(const UvPoint& uv) const;
+  Point3D ae2xyz(const AePoint& ae) const;
+  // todo: add uv2mn, ae2uv, xyz2ae
+  AePoint xyz2ae(const Point3D& xyz) const;
+  UvPoint ae2uv(const AePoint& ae) const;
+  ImagePoint uv2mn(const UvPoint& uv) const;
 };
 
-#endif // ERP_H
+#endif  // ERP_H
