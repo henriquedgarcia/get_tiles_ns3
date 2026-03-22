27e923290418346d22c16d06422519c2bd12f245
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2026-03-22 18:53:17 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,15 @@
 int main() {
   Test test;
   std::cout << "Running tests..." << std::endl;
-  std::cout << "== testing SeenTiles " << std::endl;
-  test.test_SeenTiles();
+  std::cout << std::endl << "== testing Projection " << std::endl;
+  test.test_Projection();
+  // std::cout << "== testing SeenTiles " << std::endl;
+  // test.test_SeenTiles();
+  // std::cout << std::endl << "== testing predictor " << std::endl;
+  // test.test_predictor();
 
-  std::cout << std::endl << "== testing OpenCV " << std::endl;
-  test.test_OpenCV();
+  // std::cout << std::endl << "== testing OpenCV " << std::endl;
+  // test.test_OpenCV();
   return 0;
 }
 
