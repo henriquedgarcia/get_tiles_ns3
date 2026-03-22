0a306ad1773884574186c4df181a983bc1875f78
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2026-03-22 18:52:54 -0300

    Update utils.hpp

--- include/utils.hpp
+++ include/utils.hpp
@@ -6,7 +6,6 @@
 #include <numeric>  // std::iota
 #include <opencv2/opencv.hpp>
 #include <vector>
-
 #include "types.hpp"  // Point3D
 
 inline double deg2rad(double deg) { return deg * std::numbers::pi / 180.0; }
@@ -15,13 +14,22 @@ inline double rad2deg(double rad) { return rad * 180.0 / std::numbers::pi; }
 
 inline Point3D normalize(Point3D p) { return cv::normalize(p); }
 
-inline double dot(const Point3D& a, const Point3D& b) { return a.dot(b); }
+inline double dot(const Point3D& a,
+                  const Point3D& b) {
+  return a.dot(b);
+}
 
-std::vector<Point3D> predict_future(const std::vector<Point3D>& data, int n_future = 30);
+std::vector<Point3D> predict_future(const std::vector<Point3D>& data,
+                                    int n_future = 30);
 
-std::pair<double, double> linear_regression(const std::vector<double>& y);
+std::pair<double,
+          double>
+linear_regression(const std::vector<double>& y);
 
-std::vector<double> linspace(const double start, const double end, const int num_points);
-cv::Mat applyRotation(const cv::Mat& points, const cv::Mat& R);
+std::vector<double> linspace(const double start,
+                             const double end,
+                             const int num_points);
+cv::Mat applyRotation(const cv::Mat& points,
+                      const cv::Mat& R);
 
 #endif  // UTILS_H
