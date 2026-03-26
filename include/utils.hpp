f9ab12de40ba5e7ac42b804524c859b4cfc40dce
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2026-03-26 00:16:56 -0300

    Update utils.hpp

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,39 +1,32 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include "projection.hpp"
+#include "types.hpp" // Point3D
 #include <cmath>
-#include <numbers>  // pi
-#include <numeric>  // std::iota
+#include <numbers> // pi
+#include <numeric> // std::iota
 #include <opencv2/opencv.hpp>
 #include <vector>
-#include "types.hpp"  // Point3D
 
 inline double deg2rad(double deg) { return deg * std::numbers::pi / 180.0; }
 
 inline double rad2deg(double rad) { return rad * 180.0 / std::numbers::pi; }
 
-inline double dot(const Point3D& a,
-                  const Point3D& b) {
-  return a.dot(b);
-}
+inline double dot(const Point3D &a, const Point3D &b) { return a.dot(b); }
 
-std::vector<Point3D> predict_future(const std::vector<Point3D>& data,
+std::vector<Point3D> predict_future(const std::vector<Point3D> &data,
                                     int n_future = 30);
 
-std::pair<double,
-          double>
+std::pair<double, double>
 
-linear_regression(const std::vector<double>& y);
+linear_regression(const std::vector<double> &y);
 
-std::vector<double> linspace(const double start,
-                             const double end,
+std::vector<double> linspace(const double start, const double end,
                              const int num_points);
 
-cv::Mat applyRotation(const cv::Mat& points,
-                      const cv::Mat& R);
+std::vector<Tile> make_tile_list(Projection *projection);
 
-void make_tile_list();
+std::vector<PointMN> get_tile_borders(const Tile &tile);
 
-std::vector<PointMN> get_tile_borders(const Tile& tile);
-
-#endif  // UTILS_H
+#endif // UTILS_H
