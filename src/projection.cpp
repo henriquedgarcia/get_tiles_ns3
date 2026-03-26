ca42c0740b69121a90d4809261cdb4d42a442d11
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2026-03-26 00:16:52 -0300

    Update projection.cpp

--- src/projection.cpp
+++ src/projection.cpp
@@ -1,15 +1,15 @@
 #include "projection.hpp"
+#include <opencv2/core.hpp>
 #include <vector>
 #include "types.hpp"
-
-using namespace std;
+#include "utils.hpp"
 
 Projection::Projection(const Resolution& resolution,
                        const Tiling& tiling)
     : resolution(resolution),
-      tiling(tiling) {
-  this->n_tiles         = tiling[0] * tiling[1];
-  int tile_w            = resolution[0] / tiling[0];
-  int tile_h            = resolution[1] / tiling[1];
-  this->tile_resolution = Resolution(tile_w, tile_h);
-}
+      tiling(tiling),
+      n_tiles(tiling[0] * tiling[1]),
+      tile_resolution(resolution[0] / tiling[0],
+                      resolution[1] / tiling[1]) {
+                        this->tile_list = make_tile_list(this);
+                      }
