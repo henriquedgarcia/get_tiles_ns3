6132cf58d9b62342ff55bb46eae2b78aa652cdab
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2026-04-03 15:17:23 -0300

    Update utils.hpp

--- include/utils.hpp
+++ include/utils.hpp
@@ -4,26 +4,24 @@
 #include "projection.hpp"
 #include "types.hpp" // Point3D
 #include <cmath>
-#include <numbers> // pi
-#include <numeric> // std::iota
 #include <opencv2/opencv.hpp>
 #include <vector>
 
-inline double deg2rad(double deg) { return deg * std::numbers::pi / 180.0; }
+inline double deg2rad(double deg) { return deg * PI / 180.0; }
 
-inline double rad2deg(double rad) { return rad * 180.0 / std::numbers::pi; }
+inline double rad2deg(double rad) { return rad * 180.0 / PI; }
 
 inline double dot(const Point3D &a, const Point3D &b) { return a.dot(b); }
 
-std::vector<Point3D> predict_future(const std::vector<Point3D> &data,
-                                    int n_future = 30);
+std::vector<Point3D>
+predict_future(const std::vector<Point3D> &data, int n_future = 30);
 
 std::pair<double, double>
 
 linear_regression(const std::vector<double> &y);
 
-std::vector<double> linspace(const double start, const double end,
-                             const int num_points);
+std::vector<double>
+linspace(const double start, const double end, const int num_points);
 
 std::vector<Tile> make_tile_list(Projection *projection);
 
