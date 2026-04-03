f318af35bedcebefabe5f054eb8d3febc1a734dd
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2026-04-03 15:17:28 -0300

    Update seen_tiles.cpp

--- src/seen_tiles.cpp
+++ src/seen_tiles.cpp
@@ -9,10 +9,7 @@
 #include "types.hpp"
 #include "utils.hpp"
 
-using namespace std;
-// using namespace MatRot;
-
-SeenTiles::SeenTiles(const Fov& fov, const Projection* projection) {
+SeenTiles::SeenTiles(const Fov &fov, const Projection *projection) {
   this->fov = fov;
   this->projection = projection;
   this->tiling = projection->tiling;
@@ -31,17 +28,17 @@ void SeenTiles::set_normals_default() {
   double sin_fov_h = sin(fov_h_2);
 
   // (x, y, z)
-  Frustrum frustrum(Normal(-cos_fov_w, 0.0f, -sin_fov_w),  // left
-                    Normal(cos_fov_w, 0.0f, -sin_fov_w),   // right
-                    Normal(0.0f, -cos_fov_h, -sin_fov_h),  // top
-                    Normal(0.0f, cos_fov_h, -sin_fov_h)    // bottom
+  Frustrum frustrum(Normal(-cos_fov_w, 0.0f, -sin_fov_w), // left
+                    Normal(cos_fov_w, 0.0f, -sin_fov_w),  // right
+                    Normal(0.0f, -cos_fov_h, -sin_fov_h), // top
+                    Normal(0.0f, cos_fov_h, -sin_fov_h)   // bottom
   );
   this->default_frustrum = frustrum;
 }
 
-vector<Tile> SeenTiles::get_vptiles(PointYawPitchRoll yaw_pitch_roll) {
+std::vector<Tile> SeenTiles::get_vptiles(PointYawPitchRoll yaw_pitch_roll) {
   this->yaw_pitch_roll = yaw_pitch_roll;
-  vector<Tile> vptiles;
+  std::vector<Tile> vptiles;
 
   // Se o tiling é 1x1 retorne logo o único tile.
   if (this->tiling == Tiling(1, 1)) {
@@ -52,7 +49,7 @@ vector<Tile> SeenTiles::get_vptiles(PointYawPitchRoll yaw_pitch_roll) {
   // Mat3 matrot = get_matrix(yaw_pitch_roll);
   // this->rotated_frustrum = rotate_frustrum(this->default_frustrum, matrot);
 
-  for (const Tile& tile : this->projection->tile_list) {
+  for (const Tile &tile : this->projection->tile_list) {
     if (tile_is_in_frustrum(tile)) {
       vptiles.push_back(tile);
     }
@@ -63,7 +60,7 @@ vector<Tile> SeenTiles::get_vptiles(PointYawPitchRoll yaw_pitch_roll) {
 bool SeenTiles::tile_is_in_frustrum(Tile tile) {
   /* Um tile está no frustrum se pelo menos um ponto estiver dentro do
    * frustrum */
-  for (const PointMN& point : tile.borders) {
+  for (const PointMN &point : tile.borders) {
     Point3D xyz = this->projection->mn2xyz(point);
     if (this->is_in(xyz)) {
       return true;
@@ -75,7 +72,7 @@ bool SeenTiles::tile_is_in_frustrum(Tile tile) {
 bool SeenTiles::is_in(Point3D xyz) {
   /* um ponto está dentro do frustrum se todos os produtos forem menores que
    * zero */
-  for (const Normal& normal : this->rotated_frustrum.normals) {
+  for (const Normal &normal : this->rotated_frustrum.normals) {
     double prod = dot(normal, xyz);
     if (prod > 0) {
       return false;
