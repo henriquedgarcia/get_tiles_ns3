804696a14cdbc56d6c731c79667e98a49717458d
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2026-04-03 15:17:53 -0300

    Create test_predictor.cpp

---
+++ tests/test_predictor.cpp
@@ -0,0 +1,17 @@
+
+  // void Test::test_predictor() {
+  //   std::vector<Point3D> data(30);
+  //   // preencher data com suas 30 amostras... carregar um arquivo, pode ser
+  //   um
+  //   // csv.
+  //   for (int i = 0; i < 30; ++i) {
+  //     data[i] = Point3D(i, i * 2, i * 3); // exemplo de dados
+  //   }
+
+  //   auto future = predict_future(data, 10); // prever os próximos 10 pontos
+
+  //   for (const auto &p : future) {
+  //     // std::cout << p.x << ", " << p.y << ", " << p.z << "\n";
+  //   }
+  // };
+
