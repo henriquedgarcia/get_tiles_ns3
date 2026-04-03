6551a7b349546a80f420790b1002a5837b591273
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2026-04-03 15:17:52 -0300

    Create test_OpenCV.cpp

---
+++ tests/test_OpenCV.cpp
@@ -0,0 +1,16 @@
+  // void Test::test_OpenCV() {
+  //   std::string image_path = "teste.png";
+  //   cv::Mat img = cv::imread(image_path, cv::IMREAD_COLOR);
+
+  //   if (img.empty()) {
+  //     std::cout << "Could not read the image: " << image_path << std::endl;
+  //     return;
+  //   }
+  //   std::cout << "Exibindo: " << image_path << std::endl;
+
+  //   cv::imshow("Display window", img);
+  //   int k = cv::waitKey(0); // Wait for a keystroke in the window
+
+  //   std::cout << "convertendo: " << image_path << std::endl;
+  //   cv::imwrite("starry_night.png", img);
+  // };
