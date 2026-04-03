11350f6c7f1184858dc8c2ee7199c272793f283c
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2026-04-03 15:17:55 -0300

    Create test_SeenTiles.cpp

---
+++ tests/test_SeenTiles.cpp
@@ -0,0 +1,17 @@
+  // void Test::test_SeenTiles() {
+  //   SeenTiles seen_tiles(fov, this->proj);
+  //   std::vector<Tile> vptiles = seen_tiles.get_vptiles(yaw_pitch_roll);
+
+  //   // informações sobre os tiles. fazer um loop também.
+  //   cout << "\t" << "fov = " << fov[0] << "x" << fov[1] << endl;
+  //   cout << "\t"
+  //        << "yaw_pitch_roll = (yaw=" << yaw_pitch_roll[0]
+  //        << ", pitch=" << yaw_pitch_roll[1] << ", roll=" << yaw_pitch_roll[2]
+  //        << ")" << endl;
+  //   cout << "\t" << "Number of visible tiles: " << vptiles.size() << endl;
+  //   cout << "\t" << "Visible tiles indices: ";
+  //   for (const Tile &tile : vptiles) {
+  //     cout << tile.index << " ";
+  //   };
+  //   cout << endl;
+  // };
