70ae9eca20255514385a7091723e8b4aa0f10fcc
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2026-04-03 15:17:09 -0300

    Update projection.hpp

--- include/projection.hpp
+++ include/projection.hpp
@@ -1,24 +1,25 @@
 #ifndef PROJECTION_H
 #define PROJECTION_H
 
+#include "types.hpp"
 #include <string>
 #include <vector>
-#include "types.hpp"
 
 class Projection {
- public:
-  std::vector<Tile> tile_list;
+public:
   Resolution resolution;
   Tiling tiling;
   Resolution tile_resolution;
+  std::vector<Tile> tile_list;
   uchar n_tiles;
 
-  Projection(const Resolution& resolution,
-             const Tiling& tiling);
+  Projection(const Resolution &resolution, const Tiling &tiling);
+  virtual ~Projection() = default;
 
-  virtual Point3D mn2xyz(const PointMN& mn) const  = 0;
-  virtual PointMN xyz2mn(const Point3D& xyz) const = 0;
-  virtual ~Projection()                            = default;
+  virtual Point3D mn2xyz(const PointMN &mn) const = 0;
+  virtual PointMN xyz2mn(const Point3D &xyz) const = 0;
+
+  std::vector<Tile> get_tile_list();
 };
 
-#endif  // PROJECTION_H
+#endif // PROJECTION_H
