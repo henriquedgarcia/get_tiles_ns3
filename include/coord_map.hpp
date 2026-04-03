9362f939bb720fa5ceab5f222c4c70badd53fe6a
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2026-04-03 15:16:38 -0300

    Create coord_map.hpp

---
+++ include/coord_map.hpp
@@ -0,0 +1,11 @@
+#include "types.hpp"
+
+Point3D ae2xyz(const AePoint &ae);
+AePoint xyz2ae(const Point3D &xyz);
+
+namespace erp {
+PointUV ae2uv(const AePoint &ae);
+AePoint uv2ae(const PointUV &uv);
+PointMN uv2mn(const PointUV &uv, const Resolution &resolution);
+PointUV mn2uv(const PointMN &mn, const Resolution &resolution);
+} // namespace erp
