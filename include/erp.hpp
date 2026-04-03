a8dd7d1e26cefc8f60ac17439bf1d3762e6d30d7
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2026-04-03 15:16:59 -0300

    Update erp.hpp

--- include/erp.hpp
+++ include/erp.hpp
@@ -6,8 +6,8 @@
 class ERP : public Projection {
  public:
   using Projection::Projection;
-  Point3D mn2xyz(const PointMN& mn) const override;
-  PointMN xyz2mn(const Point3D& xyz) const override;
+  Point3D mn2xyz(const PointMN &mn) const override;
+  PointMN xyz2mn(const Point3D &xyz) const override;
 };
 
-#endif  // ERP_H
+#endif // ERP_H
