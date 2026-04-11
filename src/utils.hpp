727a8564c73f1fcdcc6d11f2e633087b908b9b05
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2026-04-10 23:21:31 -0300

    Update utils.hpp

--- src/utils.hpp
+++ src/utils.hpp
@@ -13,18 +13,21 @@ inline double rad2deg(double rad) { return rad * 180.0 / PI; }
 
 inline double dot(const Point3D &a, const Point3D &b) { return a.dot(b); }
 
-std::vector<Point3D>
-predict_future(const std::vector<Point3D> &data, int n_future = 30);
+std::vector<Point3D> predict_future(const std::vector<Point3D> &data,
+                                    int n_future = 30);
 
 std::pair<double, double>
 
 linear_regression(const std::vector<double> &y);
 
-std::vector<double>
-linspace(const double start, const double end, const int num_points);
+std::vector<double> linspace(const double start, 
+                             const double end, 
+                             const int num_points);
 
 std::vector<Tile> make_tile_list(Projection *projection);
 
 std::vector<PointMN> get_tile_borders(const Tile &tile);
+
+Frustrum create_default_frustrum(Fov fov);
 
 #endif // UTILS_H
