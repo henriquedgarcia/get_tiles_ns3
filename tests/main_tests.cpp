f5f5241e8754f049dd9bfad49e5c4acb2a19f190
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2026-06-16 19:15:35 -0300

    Update main_tests.cpp

--- tests/main_tests.cpp
+++ tests/main_tests.cpp
@@ -1,8 +1,11 @@
 #include "test_SeenTiles.hpp"
 #include "test_erp_transformations.hpp"
+#include "test_viewport.hpp"
 
 int main() {
   TestErpTransformations test_erp_transformations;
   TestSeenTiles test_seen_tiles;
+  TestViewport test_viewport;
+
   return 0;
 }
