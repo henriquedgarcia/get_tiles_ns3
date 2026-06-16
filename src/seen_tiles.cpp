6bb753b143a2201ae848907998657ee303ca1aed
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2026-06-16 19:15:26 -0300

    Update seen_tiles.cpp

--- src/seen_tiles.cpp
+++ src/seen_tiles.cpp
@@ -9,11 +9,11 @@
 #include "types.hpp"
 #include "utils.hpp"
 
-SeenTiles::SeenTiles(const Fov &fov, const Projection *projection) {
-  this->fov = fov;
-  this->projection = projection;
-  this->tiling = projection->tiling;
-  this->resolution = projection->resolution;
+SeenTiles::SeenTiles(const Fov &fov, Projection &projection)
+    : fov(fov),
+      projection(projection) {
+  this->tiling = projection.tiling;
+  this->resolution = projection.resolution;
 
   this->set_normals_default();
 }
@@ -23,13 +23,13 @@ std::vector<Tile> SeenTiles::get_vptiles(PointYawPitchRoll yaw_pitch_roll) {
 
   // Se o tiling é 1x1 retorne logo o único tile.
   if (this->tiling == Tiling(1, 1)) {
-    vptiles.push_back(this->projection->tile_list[0]);
-    return vptiles;
+    Tile tile = this->projection.get_tile_list()[0];
+    vptiles.push_back(tile);
   }
 
   this->rotate_frustrum(create_quaternion(yaw_pitch_roll));
 
-  for (const Tile &tile : this->projection->tile_list) {
+  for (const Tile &tile : this->projection.get_tile_list()) {
     if (this->tile_is_in_frustrum(tile)) {
       vptiles.push_back(tile);
     }
@@ -52,7 +52,7 @@ bool SeenTiles::tile_is_in_frustrum(Tile tile) {
   /* Um tile está no frustrum se pelo menos um ponto estiver dentro do
    * frustrum */
   for (const PointMN &point : tile.borders) {
-    Point3D xyz = this->projection->mn2xyz(point);
+    Point3D xyz = this->projection.mn2xyz(point);
     if (this->is_in(xyz)) {
       return true;
     }
