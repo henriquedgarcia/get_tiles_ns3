76ce71d5370af3b1ff1f6e4ccb9655b524e4feac
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2026-06-16 19:15:22 -0300

    Update projection.cpp

--- src/projection.cpp
+++ src/projection.cpp
@@ -6,7 +6,8 @@ Projection::Projection(const Resolution &resolution, const Tiling &tiling)
     : resolution(resolution),
       tiling(tiling),
       n_tiles(tiling[0] * tiling[1]),
-      tile_resolution(resolution[0] / tiling[0], resolution[1] / tiling[1]) {}
+      tile_resolution(resolution[0] / tiling[0], resolution[1] / tiling[1]) {
+}
 
 std::vector<Tile> Projection::get_tile_list() {
   if (this->tile_list.empty()) {
